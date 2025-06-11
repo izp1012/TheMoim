ba3dc57d1824ffec79cde6a7073148003228b4cf
izp1012 <izp1012@naver.com>
2025-06-11 17:46:03 +0900

    회원가입 DTO 수정

--- payment/src/main/java/com/moim/payment/dto/usr/SignUpReqDto.java
+++ payment/src/main/java/com/moim/payment/dto/usr/SignUpReqDto.java
@@ -1,6 +1,7 @@
 package com.moim.payment.dto.usr;
 
 import com.moim.payment.domain.Usr.Usr;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -14,18 +15,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class JoinReqDto {
+public class SignUpReqDto {
 
-    //영문 숫자 길이 2~20
+    @NotBlank(message = "아이디는 필수입니다.")
     @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해주세요.")
     @NotEmpty // null or 공백일 수 없다
     private String usrname;
-    //길이 4~20
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
     @Size(min = 4, max = 20)
     @NotEmpty
     private String password;
-    //이메일형식
-    @Pattern(regexp = "^[a-zA-Z0-9]{2,20}@[a-zA-Z0-9]{2,20}\\.[a-zA-Z]{2,3}", message = "이메일 형식으로 작성해주세요.")
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]{2,20}@[a-zA-Z0-9]{2,20}\\.[a-zA-Z]{2,3}", message = "이메일 형식으로 작성해주세요.") //이메일 형식
     @NotEmpty
     private String email;
 
--- payment/src/main/java/com/moim/payment/dto/usr/SignUpRespDto.java
+++ payment/src/main/java/com/moim/payment/dto/usr/SignUpRespDto.java
@@ -5,15 +5,22 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @ToString
-public class JoinRespDto {
+public class SignUpRespDto {
+    private Long id;
     private String username;
     private String email;
+    private LocalDateTime createdAt;
 
-    public JoinRespDto(Usr usr) {
+    // 엔티티에서 DTO로 변환하는 생성자
+    public SignUpRespDto(Usr usr) {
+        this.id = usr.getId();
         this.username = usr.getUsrname();
         this.email = usr.getEmail();
+        this.createdAt = usr.getCreatedAt();
     }
 }

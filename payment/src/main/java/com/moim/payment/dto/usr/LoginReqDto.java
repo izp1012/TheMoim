2e077ed58249695d8fe261a63f3eadbf83c97bfa
izp1012 <izp1012@naver.com>
2025-06-11 17:56:14 +0900

    Login 시 Validation 추가

--- payment/src/main/java/com/moim/payment/dto/usr/LoginReqDto.java
+++ payment/src/main/java/com/moim/payment/dto/usr/LoginReqDto.java
@@ -1,5 +1,7 @@
 package com.moim.payment.dto.usr;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +14,12 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 @AllArgsConstructor
 public class LoginReqDto {
 
+    @NotBlank(message = "아이디는 필수입니다.")
+    @Size(min = 4, max = 20, message = "아이디는 4자 이상 20자 이하여야 합니다.")
     private String usrname;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Size(min = 6, max = 100, message = "비밀번호는 6자 이상 100자 이하여야 합니다.")
     private String password;
 
     public UsernamePasswordAuthenticationToken toAuthentication() {
